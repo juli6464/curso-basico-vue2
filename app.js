e4f71d838db024f7c1c6ad094afa4de9fc52f3ed
juli6464 <juli6464@hotmail.com>
2023-10-19 16:32:32 -0500

    estilos en tiempo real

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ new Vue({
       name: 'Bitcoin',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent: -10,
-
+      color: 'f4f4f4',
       price: 8400,
 
       pricesWithDays: [
@@ -26,6 +26,8 @@ new Vue({
   methods: {
     toggleShowPrices() {
       this.showPrices = !this.showPrices
+
+      this.color = this.color.split('').reverse().join('')
     }
   }
 })
